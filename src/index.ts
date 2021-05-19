1bd61b23c731cfec00073c2870487ee1e26cbd75
ThiagoPuyelli <thiagopuyelli@gmail.com>
2021-05-19 16:10:23 -0300

    emggkge

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ import { Application } from 'express'
 
 const app: Application = new App().app
 
-app.listen(app.get("port"), () => console.log('Server on Port:', app.get('port')))
+app.listen(app.get("port"), () => console.log('Server on Port:', app.get('port'), "tmkmrth"))
