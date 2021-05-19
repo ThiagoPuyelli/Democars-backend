74e6562308ad65c54df554f980631e89ecf1c51d
ThiagoPuyelli <thiagopuyelli@gmail.com>
2021-05-19 16:13:15 -0300

    husky configurado

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ import { Application } from 'express'
 
 const app: Application = new App().app
 
-app.listen(app.get("port"), () => console.log('Server on Port:', app.get('port'), "tmkmrth"))
+app.listen(app.get('port'), () => console.log('Server on Port:', app.get('port')))
